d2217eab0160328f74f71c8b883bf4f8bf6dd827
Alice (BedinAV) <bedintema@gmail.com>
2023-07-07 02:09:10 +0300

    code: вывод максимума

--- main.cpp
+++ main.cpp
@@ -7,6 +7,8 @@ int main()
     cout << "Enter A and B: ";
     int a, b;
     cin >> a >> b;
+    if (a > b) cout << a;
+    else cout << b;
     cout << "A + B = " << a + b << '\n'
     << "A - B = " << a - b << '\n'
     << "A * B = " << a * b << '\n'

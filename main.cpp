16db3d08b770418ee742e3a3c45efba73f99aff5
Bob (BedinAV) <bedintema@gmail.com>
2023-07-07 05:51:40 +0300

    code: разрешение слияния

--- main.cpp
+++ main.cpp
@@ -8,6 +8,7 @@ int main()
     int a, b;
     cin >> a >> b;
     if (a > b) cout << a;
+    if (a < b) cout << a;
     else cout << b;
     cout << "A + B = " << a + b << '\n'
     << "A - B = " << a - b << '\n'
